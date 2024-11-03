6c7f962fc2bf20f32726a9f4a0d27137076f26b5
Paulo Alves <paulo.alves@everi.com>
2024-11-03 13:09:42 +0100

    Removing start pages; Adding recipies and planner pages

--- recipies-app/src/app/tabs/tabs-routing.module.ts
+++ recipies-app/src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,23 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'recipies',
+        loadChildren: () => import('../pages/recipies/recipies.module').then( m => m.RecipiesPageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'planner',
+        loadChildren: () => import('../pages/planner/planner.module').then(m => m.PlannerPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/recipies',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/recipies',
     pathMatch: 'full'
   }
 ];
